2736c4cfe39eb4c0367e44f4693defadbae1ad10
gmnaik <gmnaik96@gmail.com>
2025-01-28 22:21:51 +0530

    Model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -134,44 +134,6 @@ class DataTransformation:
             
             save_object(file_path = self.data_transformation_config.preprocessor_obj_file_path,obj=preprocessing_obj)
             
-            '''
-            
-            logging.info("Obtaining preprocessing object")
-            
-            preprocessing_obj = self.get_data_transformer_object()
-            
-            target_column_name="math_score"
-            #numerical_columns = ["writing_score","reading_score"]
-            
-            input_feature_train_df = train_df.drop(columns=target_column_name,axis=1)
-            target_feature_train_df = train_df[target_column_name]
-            
-            input_feature_test_df = test_df.drop(columns=target_column_name,axis=1)
-            target_feature_test_df = test_df[target_column_name]
-            
-            print("input_feature_train_df arr:\n",input_feature_train_df)
-            print("input_feature_test_df arr:\n",input_feature_test_df)
-            
-            logging.info("Applying preprocessing object on training dataframe and testing dataframe")
-            
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
-            
-            print("input_feature_train_arr arr:\n",input_feature_train_arr)
-            print("input_feature_test_arr arr:\n",input_feature_test_arr)
-            
-            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
-            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
-            
-            print("Train arr:\n",train_arr)
-            print("Test arr:\n",test_arr)
-            
-            logging.info("Saved preprocessing object.")
-            
-            save_object(file_path = self.data_transformation_config.preprocessor_obj_file_path,obj=preprocessing_obj)
-            '''
-            #train_arr = [1,2,3]
-            #test_arr = [4,5,6]
             return(
                 train_arr,
                 test_arr,
